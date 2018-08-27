cd69003372d73df626ec0937229b44f252262c38
aalexander3 <aralx73@gmail.com>
2018-08-26 22:14:20 -0400

    thinking about components

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux'
 class HomePage extends Component{
 
   state = {
-    currentPage: 0,
+    currentIndex: 0,
     interval: 0
   }
 
@@ -26,31 +26,36 @@ class HomePage extends Component{
   }
 
   nextPage = () => {
-    // rotates through 0 - 4 and sets the currentPage
+    // rotates through 0 - 4 and sets the currentIndex
     this.setState(prevState => {
-      if (prevState.currentPage === 3){
-        return { currentPage: 0 }
+      if (prevState.currentIndex === 3){
+        return { currentIndex: 0 }
       } else {
-        return { currentPage: prevState.currentPage + 1 }
+        return { currentIndex: prevState.currentIndex + 1 }
       }
     })
   }
 
-  changePage = (e) => {
+  changePage = e => {
     // onClick changes the current page, as well as resets the interval to for nextPage
     const index = this.pageMap.indexOf(e.target.dataset.name)
     this.setState({
-      currentPage: index
+      currentIndex: index
     }, this.clearAndSet)
+  }
+
+  goToPage = e => {
+    // push onto window history the new page
+    console.log(e.target.dataset.name)
   }
 
   pageMap = ['home', 'projects', 'about', 'contact']
 
   render(){
-    const current = this.props.home[this.pageMap[this.state.currentPage]]
+    const current = this.props.home[this.pageMap[this.state.currentIndex]]
     const makeCircles = this.pageMap.map(page => {
       return (<div
-          className={"small-circle" + ((this.pageMap[this.state.currentPage] === page) ? ' active' : '')}
+          className={"small-circle" + ((this.pageMap[this.state.currentIndex] === page) ? ' active' : '')}
           key={page}
           data-name={page}
           onClick={this.changePage}
@@ -61,7 +66,11 @@ class HomePage extends Component{
 
     return (
       <div className={"page " + current.color}>
-        <div className="page-card">
+        {/* make this into a component? then it'll know about props */}
+        <div className="page-card"
+          onClick={this.goToPage}
+          data-name={this.pageMap[this.state.currentIndex]}
+        >
           <h1>{current.headline}</h1>
           <h3>{current.description}</h3>
         </div>
