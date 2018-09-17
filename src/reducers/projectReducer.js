85ea181c2afaac040d95dab96f32aa3515ec33c2
aalexander3 <aralx73@gmail.com>
2018-09-17 17:27:37 -0400

    added more project images

--- src/reducers/projectReducer.js
+++ src/reducers/projectReducer.js
@@ -14,15 +14,15 @@ const projectState = {
       description: 'Investr is a React, Redux, and Rails application that pairs investors with startups looking for funding. The startup data was scraped from Angel\'s List. The Real-time messaging feature was implemented with Action Cable.',
       github: 'https://github.com/aalexander3/venture-time-frontend',
       demo: null,
-      images: ['ForecastVR-vr.png', 'ForecastVR-landing.png', 'ForecastVR-detail.png'],
+      images: ['investr-match.png', 'investr-message.png', 'investr-profile.png'],
       slug: 'investr'
     },
     {
       name: "Wantr",
-      description: 'Forecast VR is a React, Redux, and AFrame web application that renders a virtual reality representation of current conditions of 21 major cities around the world. The application uses the Dark Sky API for its source of weather information.',
-      github: 'https://github.com/aalexander3/forecast-vr-frontend',
+      description: 'Wantr is a productivity dashboard designed to help users keep track of their goals. The application is built on a Rails API backend with a vanilla JavaScript frontend.',
+      github: 'https://github.com/aalexander3/wantr',
       demo: null,
-      images: ['ForecastVR-vr.png', 'ForecastVR-landing.png', 'ForecastVR-detail.png'],
+      images: ['wantr-land.png', 'wantr-dash.png', 'wantr-add.png'],
       slug: 'wantr'
     },
   ],
