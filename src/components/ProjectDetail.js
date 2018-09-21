e1512e6bf49cd79b5883b831ead9a374e5051812
aalexander3 <aralx73@gmail.com>
2018-09-20 20:22:19 -0400

    project detail improved

--- src/components/ProjectDetail.js
+++ src/components/ProjectDetail.js
@@ -20,7 +20,8 @@ class ProjectDetail extends Component {
         <div className="project-info">
           <h1>{name}</h1>
           <h4>{description}</h4>
-          <p>{github} | {demo}</p>
+          <a href={github} target="_blank" rel="noopener noreferrer" >GITHUB</a>
+          { demo ? <a href={demo} target="_blank" rel="noopener noreferrer" >DEMO</a> : null }
         </div>
       </div>
     )
